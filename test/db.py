0fc7f0856e182b9a438f48a5b3541af623c2bd44
李文杰 <JackJay@MBPJAY.local>
2016-10-12 00:25:19 +0800

    小改

--- test/db.py
+++ test/db.py
@@ -104,7 +104,7 @@ def save(data,session):
 def test():
     DATA = '''60,204000002126,1476151500000,4294967295,b'000857',b'2016-10-11',b'2016-10-11 10:05:00.000',1476151500000,b'SSE',500医药,13255.13,13256.477,13255.13,13255.299,12285.0,25170952.0,13263.522,-0.812,-0.0001,662841,0.0,13233.555,13273.289,0.0,{},{}'''
     DATA = DATA.format(time.time(),datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    DATA = DATA.decode('utf-8')
+    # DATA = DATA.decode('utf-8')
     CONN = TEST_REMOTE_CONN if platform.system() == 'Darwin' else TEST_LOCAL_CONN
     engine = create_engine(CONN)
     testEngine = create_engine(CONN)
