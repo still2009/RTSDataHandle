14166cd978a704f13b7ce06b081f414d0d04286f
李文杰 <JackJay@MBPJAY.local>
2016-10-14 00:53:23 +0800

    小改bug

--- test/db_process.py
+++ test/db_process.py
@@ -6,12 +6,12 @@ from db import *
 class ConsumeThread(threading.Thread):
     '''数据消费线程'''
     def __init__(self,type):
-        ''''
+        '''
         type 代表线程类型，有两种：
         1. 最终分时数据 -> final
         2. 中间分时数据 -> middle
         最终分时数据将会被优先写入数据库
-        ''''
+        '''
         threading.Thread.__init__(self)
         self.Priority = 1 if type == 'final' else 0
         self.delay = 15 if self.Priority == 1 else 150
