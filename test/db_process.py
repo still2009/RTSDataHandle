b51e8943ec26d72a807c3bf9f3617771d2ae9540
李文杰 <JackJay@MBPJAY.local>
2016-10-14 14:02:12 +0800

    线程专属日志文件，下一步准备多线程

--- test/db_process.py
+++ test/db_process.py
@@ -1,17 +1,10 @@
 # coding:UTF-8
+# import multiprocessing
 import threading
 import time
 from db import *
 import logging
 
-logFname = 'thread_%s.log' % datetime.datetime.now().strftime('%y%m%d_%H_%M_%S')
-logging.basicConfig(level=logging.INFO,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%b %d  %H:%M:%S',
-                filename=logFname,
-                filemode='w')
-def log(txt):
-    logging.info(txt)
 class ConsumeThread(threading.Thread):
     '''数据消费线程'''
     def __init__(self,type,SessionClass,finalDelay=35,middleDelay=15):
@@ -26,25 +19,33 @@ class ConsumeThread(threading.Thread):
         self.delay = finalDelay if self.Priority == 1 else middleDelay
         self.SessionClass = SessionClass
         self.dbSession = SessionClass()
-
         self.AddFlag = True
+        self.logger = logging.getLogger('p:%s thread' % self.Priority)
+        self.logger.setLevel(logging.INFO)
+        logFname = './thread_%s_%s.log' % (self.Priority,datetime.datetime.now().strftime('%y%m%d_%H_%M_%S'))
+        fh = logging.FileHandler(logFname)
+        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
+    def log(self,txt):
+        self.logger.info(txt)
     def run(self):
-        log('p:%s Started' % self.Priority)
+        self.log('p:%s Started' % self.Priority)
         while True:
             time.sleep(self.delay)
             # 开始换session
             self.AddFlag = False
             sLen = len(self.dbSession.new)
             if sLen <= 0:
-                log('p-%s empty!!' % self.Priority)
+                self.log('p-%s empty!!' % self.Priority)
                 continue
             s = self.dbSession
             self.dbSession = self.SessionClass()
             self.AddFlag = True
             # 换session成功
-            log('p:%s is commiting %s items' % (self.Priority,sLen))
+            self.log('p:%s is commiting %s items' % (self.Priority,sLen))
             s.commit()
-            log('p:%s commited %s items' % (self.Priority,sLen))
+            self.log('p:%s commited %s items' % (self.Priority,sLen))
             s.close()
     def add(self,data):
         while True:
